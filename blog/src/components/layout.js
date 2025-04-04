b11f44a9fe93755e938ab20462126c9e9b1dac36
Madison Weitz <weitzm@gmatc.matc.edu>
2025-04-03 20:02:42 -0500

    Implemented new Main and Footer Rebass Components in Layout

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -5,16 +5,13 @@ import styled, { ThemeProvider } from "styled-components"
 import { Header } from "./Header"
 import "./layout.css"
 import { Gray } from "./themes/Gray"
+import { Main } from "./Main"
+import { Footer } from "./Footer"
 
 const Content = styled.div`
   margin: 0 auto;
   max-width: var(--size-content);
   padding: var(--size-gutter);
-`
-
-const Footer = styled.footer`
-  margin-top: var(--space-5);
-  font-size: var(--font-sm);
 `
 
 const Layout = ({ children }) => {
@@ -32,8 +29,13 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={Gray} >
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <Footer>
+        <Main>{children}</Main>
+        <Footer
+          style={{
+            marginTop: `var(--space-5)`,
+            fontSize: `var(--font-sm)`,
+          }}
+        >
           © {new Date().getFullYear()} &middot; Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
